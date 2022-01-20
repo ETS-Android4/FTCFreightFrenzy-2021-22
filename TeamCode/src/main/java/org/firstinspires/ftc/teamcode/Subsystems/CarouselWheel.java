50093e99fd139fbc6400f6448762d3710d9214a7
cyberLeelad <rachel.dzwonkowski@gmail.com>
2022-01-20 15:17:12 -0500

    Update CarouselWheel.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/CarouselWheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/CarouselWheel.java
@@ -1,14 +1,15 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.Utility.*;
 
 public abstract class CarouselWheel extends Subsystem {
-    public static DcMotorEx carouselMotor;
+    public static CRServo carouselMotor;
 
     public static void initialize() {
-        carouselMotor = hm.get(DcMotorEx.class, "CarouselMotor");
+        carouselMotor = hm.get(CRServo.class, "CarouselMotor");
     }
 
     public static void setPower(double pow){
